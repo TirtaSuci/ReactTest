fdf3e8ede4708bcd1bf3cac5e281c09282f7f512
TirtaSuci <tirtas43@gmail.com>
2025-02-16 12:26:09 +0700

    update

--- src/component/context/DarkMode.jsx
+++ src/component/context/DarkMode.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const DarkModeContext = createContext();
 
 const DarkModeContextProvider = ({ children }) => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        return JSON.parse(localStorage.getItem("darkMode")) || false;
+    });
+
+    useEffect(() => {
+        localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
+    }, [isDarkMode]);
+
     return (
         <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
             {children}
